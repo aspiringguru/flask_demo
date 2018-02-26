361fc242603ba723fea9dcf455bb3a1b19f14387
aspiringguru <bmatthewtaylor@gmail.com>
2018-02-26 12:23:19 +1000

    wip added

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -22,3 +22,15 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
